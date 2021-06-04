8cd5405d988d5d53c2604d8130c27be9dfe9381d
Frank <frankstam01@gmail.com>
2021-06-04 21:45:59 +0200

    Added new kit: NightOwl

---
+++ src/main/java/me/libraryaddict/librarys/Abilities/NightOwl.java
@@ -0,0 +1,18 @@
+package me.libraryaddict.librarys.Abilities;
+
+import org.bukkit.event.EventHandler;
+
+import me.libraryaddict.Hungergames.Abilities.Ninja;
+import me.libraryaddict.Hungergames.Events.GameStartEvent;
+
+public class NightOwl extends Ninja {
+    public String[] potionEffectsDuringDay = new String[] { "NIGHT_VISION 0" };
+    public String[] potionEffectsDuringNight = new String[] { "NIGHT_VISION 0"};
+
+    @EventHandler
+    public void gameStartEvent(GameStartEvent event) {
+        super.potionEffectsDuringDay = potionEffectsDuringDay;
+        super.potionEffectsDuringNight = potionEffectsDuringNight;
+        super.gameStartEvent(event);
+    }
+}
